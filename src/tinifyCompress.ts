92086ec30d4d209908dc4b94563d7f0660949bc0
Simon he <13917107460@163.com>
2022-08-03 10:08:26 +0800

    chore: add size change

--- src/tinifyCompress.ts
+++ src/tinifyCompress.ts
@@ -31,29 +31,30 @@ export async function tinifyCompress() {
       chokidar.watch(path.resolve(cwd(), directory)).on('all', async (event, pathDir) => {
         if (event === 'add') {
           const data = await fs.readFileSync(pathDir)
+          const originalSize = `${(data.length / 1024).toFixed(2)}kb`
           const spinner = ora({ text: `${pathDir}`, color: 'yellow', spinner: 'aesthetic' }).start()
           if (isEqual(data.slice(-10), buf))
             return spinner.succeed(`${pathDir} already compressed 🎉`)
           const { mime = '' } = await fileTypeFromFile(pathDir) || {}
           if (mime === 'image/gif')
-            return compressGif(pathDir, spinner)
+            return compressGif(pathDir, spinner, originalSize)
           if (!types.includes(mime))
             return spinner.fail(`mime ${mime} is not supported`)
-          compressImage(pathDir, spinner)
+          compressImage(pathDir, spinner, originalSize)
         }
       })
     })
   })
-  function compressImage(pathDir: string, spinner: any) {
+  function compressImage(pathDir: string, spinner: any, originalSize?: string) {
     const source = tinify.fromFile(pathDir)
     const copyrighted = source.preserve('copyright', 'creation')
     copyrighted.toFile(pathDir, (err) => {
       if (err)
         return spinner.fail(err?.message)
-      addFlag(pathDir, spinner)
+      addFlag(pathDir, spinner, originalSize)
     })
   }
-  async function compressGif(pathDir: string, spinner: any) {
+  async function compressGif(pathDir: string, spinner: any, originalSize?: string) {
     const { data } = (await imagemin([pathDir], {
       plugins: [
         imageminGifsicle({
@@ -63,7 +64,7 @@ export async function tinifyCompress() {
     }))?.[0]
 
     try {
-      addFlag(pathDir, spinner, data)
+      addFlag(pathDir, spinner, data, originalSize)
       spinner.succeed(`${pathDir} compressed successfully`)
     }
     catch (error: any) {
@@ -77,7 +78,11 @@ export async function tinifyCompress() {
     }
     return true
   }
-  function addFlag(pathDir: string, spinner: any, source?: Buffer) {
+  function addFlag(pathDir: string, spinner: any, source?: Buffer | string, originalSize?: string) {
+    if (typeof source === 'string') {
+      originalSize = source
+      source = undefined
+    }
     const data = source ?? fs.readFileSync(pathDir)
     const k = new Uint8Array(data.length + buf.length)
     for (let i = 0; i < data.length; i++)
@@ -85,9 +90,9 @@ export async function tinifyCompress() {
 
     for (let i = 0; i < buf.length; i++)
       k[data.length + i] = buf[i]
-
+    const newSize = `${(k.length / 1024).toFixed(2)}kb`
     fs.writeFileSync(pathDir, k)
-    spinner.succeed(`${pathDir} compressed successfully`)
+    spinner.succeed(`${pathDir} compressed successfully ${originalSize} => ${newSize}`)
   }
 }
 
